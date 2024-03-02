a83092414d5f89c5135a1c2807c74f8c7a681a71
shreeyam-01 <132832581+shreeyam-01@users.noreply.github.com>
2024-03-02 16:39:25 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+const listContainer = document.getElementById("list-container")
+const inputBox = document.getElementById("input-box")
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("You must write something!")
+    }
+
+    else{
+        let li = document.createElement("li")
+        li.innerHTML = inputBox.value
+        listContainer.appendChild(li)
+
+        let span = document.createElement("span")
+        span.innerHTML = "\u00d7"
+        li.appendChild(span)
+    }
+
+    inputBox.value = ''
+    savedata()
+}
+
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked")
+        savedata()
+    }
+
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove()
+        savedata()
+    }
+    
+},false)
+
+function savedata(){
+    localStorage.setItem("data",listContainer.innerHTML)
+}
+
+function show(){
+    listContainer.innerHTML = localStorage.getItem("data")
+}
+
+show()
